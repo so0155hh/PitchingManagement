b0d19286aa0ce77b4f385dcfcbcd5be3fae9a3a5
Nozomi <nozomi.kuwahara@gmail.com>
2020-03-05 18:35:33 +0900

    first

---
+++ PitchingManagement/CalendarVars.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarVars.swift
+//  PitchingManagement
+//
+//  Created by 桑原望 on 2020/03/05.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import Foundation
---
+++ PitchingManagement/RecordCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecordCollectionViewCell.swift
+//  PitchingManagement
+//
+//  Created by 桑原望 on 2020/02/28.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import UIKit
+
+class RecordCollectionViewCell: UICollectionViewCell {
+    
+}
